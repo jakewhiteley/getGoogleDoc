2936701ec81df00ba16d02263d4fe873be78c39a
jake whiteley <odegramusic@gmail.com>
2014-12-01 10:59:54 +0000

    Moved results array to within parseEntry closure
    Simultaneous calls to getGoogleDoc would not return all spreadsheets as the reference to results was being kept.

--- src/getGoogleDoc.js
+++ src/getGoogleDoc.js
@@ -4,48 +4,43 @@
 */
 (function ( $ ) {
 	$.extend({
-		getGoogleDoc: function (urlKey, callback) {
-			var self = this,
-				// results holder
-				results = []; 
+		getGoogleDoc: function (urlKey, callback, results) {
+			var self = this; 
 
 			// converts the return data from Google into a usable array
-			this.parseEntry = function (a) {
+			this.parseEntry = function (ab, results) {
 				var keys = [];
 				// get keys
-				for (k in a[0]) 
+				for (k in ab[0]) 
 					if (k.match(/gsx/) !== null) 
 						keys.push(k.replace('gsx$', ''));
 				// create results array
-				for (var i = 0, t = {}; i < a.length; t = {}, i++) {
+				for (var i = 0, t = {}; i < ab.length; t = {}, i++) {
 					for (k in keys) 
 						if (keys.hasOwnProperty(k)) {
-							var result = (a[i]['gsx$' + keys[k]]['$t'].length > 0 ? a[i]['gsx$' + keys[k]]['$t'] : null);
+							var result = (ab[i]['gsx$' + keys[k]]['$t'].length > 0 ? ab[i]['gsx$' + keys[k]]['$t'] : null);
 							// push result to results holder, type cast numbers if needed
 							t[ keys[k] ] = ( !isNaN( parseFloat(result) ) ? parseFloat(result) : result );
 						}
 					// add result to holder
 					results.push(t);
 				}
+				return results;
 			};
 
 			// make GET request, and return promise for chaining
 			return $.getJSON(
 				"https://spreadsheets.google.com/feeds/list/"+urlKey+"/od6/public/values?alt=json&callback=?", 
-				function (a, b, c) {
+				function (a) {
 					// if the sheet was empty 
-					if (typeof a.feed.entry === 'undefined') {
+					if (typeof a.feed.entry === 'undefined') 
 						return [];
-					}
-					else {
-						// parse the results
-						self.parseEntry(a.feed.entry);
+					else 
 						// callback
 						if (typeof callback == 'function')
-							callback(results);
-					}					
+							callback(self.parseEntry(a.feed.entry, []));
 				}
 			);
 		}
 	});
-}( jQuery ));
+}(jQuery));
